2f5128e332700bb09b80d05a7340ee05902b1a46
nick <sinzihlin@gmail.com>
2020-08-22 13:05:00 +0800

    add BuleThooth 專案

---
+++ Tech_project/Bluethooth/Bluethooth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bluethooth
+//
+//  Created by SinZih Liou on 2020/8/22.
+//  Copyright © 2020 nickliu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
